18ff4022568d225283df48830645a01b60b72bd1
Adrian Lafond <mail@adrianlafond.com>
2021-08-28 01:38:11 -0400

    complete search and test

--- src/search.ts
+++ src/search.ts
@@ -1,8 +1,33 @@
+import { lang as defaultLang } from './settings';
+
 export const API = 'https://api.dictionaryapi.dev/api/v2/entries';
 
-export const apiUrl = (word: string, lang = 'en') => `${API}/${lang}/${word}`;
+export const apiUrl = (word: string, lang = defaultLang) => `${API}/${lang}/${word}`;
 
-export function search(word: string, lang = 'en') {
+export interface apiResult {
+  word: string;
+  phonetic: string;
+  phonetics: { text: string; audio?: string }[];
+  origin: string;
+  meanings: {
+    partOfSpeech: string;
+    definitions: {
+      definition: string;
+      example: string;
+      synonyms: string[];
+      antonyms: string[];
+    }[];
+  }[];
+};
+
+export interface searchResult {
+  error?: number;
+  data?: apiResult[];
+}
+
+export function search(word: string, lang = defaultLang): Promise<searchResult> {
   return fetch(apiUrl(word, lang))
-    .then(resp => resp.json());
+    .then(resp => resp.status === 200 ? resp.json() : resp.status)
+    .then(json => (typeof json === 'number' ? { error: json } : { data: json }))
+    .catch(() => ({ error: 400 }));
 }
--- test/search.spec.ts
+++ test/search.spec.ts
@@ -11,13 +11,87 @@ describe('apiUrl()', () => {
 });
 
 describe('search()', () => {
-  it('mocks a successful response', async () => {
-    console.log(apiUrl('test'));
-    fetchMock.mock(apiUrl('test'), {
-      body: { foo: 'bar' },
+  const successResponse = [
+    {
+      "word": "hello",
+      "phonetic": "həˈləʊ",
+      "phonetics": [
+        {
+          "text": "həˈləʊ",
+          "audio": "//ssl.gstatic.com/dictionary/static/sounds/20200429/hello--_gb_1.mp3"
+        },
+        {
+          "text": "hɛˈləʊ"
+        }
+      ],
+      "origin": "early 19th century: variant of earlier hollo ; related to holla.",
+      "meanings": [
+        {
+          "partOfSpeech": "exclamation",
+          "definitions": [
+            {
+              "definition": "used as a greeting or to begin a phone conversation.",
+              "example": "hello there, Katie!",
+              "synonyms": [],
+              "antonyms": []
+            }
+          ]
+        },
+        {
+          "partOfSpeech": "noun",
+          "definitions": [
+            {
+              "definition": "an utterance of ‘hello’; a greeting.",
+              "example": "she was getting polite nods and hellos from people",
+              "synonyms": [],
+              "antonyms": []
+            }
+          ]
+        },
+        {
+          "partOfSpeech": "verb",
+          "definitions": [
+            {
+              "definition": "say or shout ‘hello’.",
+              "example": "I pressed the phone button and helloed",
+              "synonyms": [],
+              "antonyms": []
+            }
+          ]
+        }
+      ]
+    }
+  ];
+
+  afterEach(() => {
+    fetchMock.restore();
+  });
+
+  it('returns a successful response', async () => {
+    fetchMock.mock(apiUrl('hello'), {
+      body: successResponse,
       status: 200,
     });
+    const res = await search('hello');
+    expect(res.data?.[0].word).toBe('hello');
+    expect(res.error).toBeUndefined();
+  });
+  it('returns an appropriate error for not found', async () => {
+    fetchMock.mock(apiUrl('test'), 404);
     const res = await search('test');
-    expect(res.foo).toBe('bar');
+    expect(res.error).toBe(404);
+  });
+  it('returns an appropriate error for server error', async () => {
+    fetchMock.mock(apiUrl('test'), 500);
+    const res = await search('test');
+    expect(res.error).toBe(500);
+  });
+  it('returns an error when fetch throws an error', async () => {
+    fetchMock.catch
+    fetchMock.mock(apiUrl('test'), {
+      throws: new Error('fetch failed!')
+    });
+    const res = await search('test');
+    expect(res.error).toBe(400);
   });
 });
